66a0248a81d7540743a3ef9c3f2d702df1962c86
hatemBay <bayoudhhatem5@gmail.com>
2023-08-19 16:13:59 +0100

    Initial commit

---
+++ apps/nest-microservices-playground/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'nest-microservices-playground',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nest-microservices-playground',
+};
