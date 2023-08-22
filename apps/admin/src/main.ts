0f6b5a046206b12edad44bffdaac5a181ec34b79
hatemBay <bayoudhhatem5@gmail.com>
2023-08-22 15:48:44 +0100

    global pipe for class validation, enabled CORS for angular

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -3,15 +3,19 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhohst:4200',
+  });
   // const globalPrefix = 'api';
   // app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
   await app.listen(3000);
   Logger.log(
