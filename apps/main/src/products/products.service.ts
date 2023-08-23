eb628199c74717c3843b1714eb912ca5a087283c
hatemBay <bayoudhhatem5@gmail.com>
2023-08-23 17:26:57 +0100

    added RMQ connection between microservices, sent events from admin to main

--- apps/main/src/products/products.service.ts
+++ apps/main/src/products/products.service.ts
@@ -3,8 +3,8 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Product } from './entities/product.entity';
-import { Model } from 'mongoose';
 import { IProduct } from './interfaces/product.interface';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ProductsService {
@@ -20,6 +20,13 @@ export class ProductsService {
     return await newProduct.save();
   }
 
+  async createFromEvent(product: IProduct): Promise<IProduct> {
+    const newProduct = new this.productModel(product);
+
+    const savedProduct = await newProduct.save();
+    return savedProduct;
+  }
+
   async findAll(): Promise<IProduct[]> {
     const products = await this.productModel.find();
     if (!products || products.length == 0) {
@@ -28,7 +35,7 @@ export class ProductsService {
     return products;
   }
 
-  async findOne(id: number): Promise<IProduct> {
+  async findOne(id: string): Promise<IProduct> {
     const product = await this.productModel.findById(id);
     if (!product) {
       throw new NotFoundException(`Product ${id} not found`);
@@ -37,7 +44,7 @@ export class ProductsService {
   }
 
   async update(
-    id: number,
+    id: string,
     updateProductDto: UpdateProductDto
   ): Promise<IProduct> {
     const existingProduct = await this.productModel.findByIdAndUpdate(
@@ -51,8 +58,33 @@ export class ProductsService {
     return existingProduct;
   }
 
-  async remove(id: number): Promise<IProduct> {
+  async updateFromEvent(product: IProduct): Promise<IProduct> {
+    const existingProduct = await this.productModel.findOneAndUpdate(
+      { id: product.id },
+      product,
+      { new: true }
+    );
+    if (!existingProduct) {
+      throw new NotFoundException(`Product ${product.id} not found`);
+    }
+    console.log(existingProduct);
+
+    return existingProduct;
+  }
+
+  async remove(id: string): Promise<IProduct> {
     const productToDelete = await this.productModel.findByIdAndDelete(id);
+
+    if (!productToDelete) {
+      throw new NotFoundException(`Product ${id} not found`);
+    }
+    return productToDelete;
+  }
+
+  async removeFromEvent(id: number): Promise<IProduct> {
+    const productToDelete = await this.productModel.findOneAndDelete({
+      id,
+    });
     if (!productToDelete) {
       throw new NotFoundException(`Product ${id} not found`);
     }
