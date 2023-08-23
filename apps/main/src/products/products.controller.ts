c54e2d6d88db32d336f714a565c06be1db15974f
hatemBay <bayoudhhatem5@gmail.com>
2023-08-23 17:23:14 +0100

    made methods async

--- apps/main/src/products/products.controller.ts
+++ apps/main/src/products/products.controller.ts
@@ -10,33 +10,53 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { EventPattern } from '@nestjs/microservices';
+import { IProduct } from './interfaces/product.interface';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  async create(@Body() createProductDto: CreateProductDto): Promise<IProduct> {
+    return await this.productsService.create(createProductDto);
   }
 
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  async findAll(): Promise<IProduct[]> {
+    return await this.productsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<IProduct> {
+    return await this.productsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto
+  ): Promise<IProduct> {
+    return await this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  async remove(@Param('id') id: string): Promise<IProduct> {
+    return await this.productsService.remove(id);
+  }
+
+  @EventPattern('product_created')
+  async handleCreatedProduct(product: IProduct): Promise<IProduct> {
+    return await this.productsService.createFromEvent(product);
+  }
+
+  @EventPattern('product_updated')
+  async handleUpdatedProduct(product: IProduct): Promise<IProduct> {
+    return await this.productsService.updateFromEvent(product);
+  }
+
+  @EventPattern('product_deleted')
+  async handleDeletedProduct(id: number): Promise<IProduct> {
+    return await this.productsService.removeFromEvent(id);
   }
 }
