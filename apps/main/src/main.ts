fc3e3c1f1785e4b5d8f2387e5f9a51b53fd88754
hatemBay <bayoudhhatem5@gmail.com>
2023-08-23 17:32:00 +0100

    updated to a hybrid microservice

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -7,17 +7,32 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhohst:4200',
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        'amqps://wffroruz:9Au6TXmNSy8GSlUI7QklebOu-CJiXkn_@goose.rmq2.cloudamqp.com/wffroruz',
+      ],
+      queue: 'main_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
   });
+  // app.enableCors({
+  //   origin: 'http://localhohst:4200',
+  // });
   // const globalPrefix = 'api';
   // app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3001;
+  await app.startAllMicroservices();
   await app.listen(port);
+  Logger.log('🚀 Microservice is listening');
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
